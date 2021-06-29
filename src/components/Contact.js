c54ce7697d0efc012e21ec3de44eddbff67d0cd9
patrickjgabr <patrickjgabr@gmail.com>
2021-06-29 14:45:01 +1000

    reset contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,50 +12,6 @@ function Contact(props) {
       <Heading level={3} margin={{ top: "none", bottom: "small" }}>
         Contact
       </Heading>
-      <Container style={{ width: "100%" }}>
-        <Row>
-          <Col xs={12} md={4}>
-            <Box>Text</Box>
-          </Col>
-          <Col xs={12} md={8}>
-            <Box>
-              <Container style={{ marginLeft: 0, marginRight: 0 }}>
-                <Row>
-                  <Col xs={12} md={6}>
-                    Name
-                    <TextInput></TextInput>
-                  </Col>
-                  <Col xs={12} md={6}>
-                    Email
-                    <TextInput></TextInput>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col>
-                    <Text margin={{ bottom: "large" }}>Message</Text>
-                  </Col>
-                  <Col xs={12} md={12}>
-                    <TextArea
-                      resize={false}
-                      style={{ height: "250px" }}
-                    ></TextArea>
-                  </Col>
-                </Row>
-                <Row>
-                  <Button
-                    label="submit"
-                    style={{
-                      marginRight: "auto",
-                      marginTop: 20,
-                      marginLeft: "auto",
-                    }}
-                  ></Button>
-                </Row>
-              </Container>
-            </Box>
-          </Col>
-        </Row>
-      </Container>
     </Box>
   );
 }
