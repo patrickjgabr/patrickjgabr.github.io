5d5be8c5d11f4c89f1453cbb45652ed7a0608eb7
patrickjgabr <patrickjgabr@gmail.com>
2021-07-09 13:05:14 +1000

    seperate blogs and projects

--- functions/getBlog.js
+++ functions/getBlog.js
@@ -1,21 +1,6 @@
 const fetch = require("node-fetch");
 
 exports.handler = async function (event, context) {
-  let q;
-  if (event.queryStringParameters.q == "projects") {
-    q = process.env.ProjectsDB;
-  }
-  if (event.queryStringParameters.q == "blog") {
-    q = process.env.BlogDB;
-  }
-
-  if (q == undefined) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ message: "Invalid Query" }),
-    };
-  }
-
   const headers = {
     method: "POST",
     credentials: "include",
@@ -25,25 +10,21 @@ exports.handler = async function (event, context) {
     },
   };
 
-  const url = "https://api.notion.com/v1/databases/" + q + "/query";
+  const url =
+    "https://api.notion.com/v1/databases/" + process.env.BlogDB + "/query";
 
   return fetch(url, headers)
     .then((res) => res.json())
     .then((data) => {
       let pages = data.results.map((page) => {
+        if (page.properties.Status.multi_select[0].name != "Complete") {
+          return;
+        }
         let result = {};
         result.name = page.properties.Name.title[0].plain_text;
-        result.status = page.properties.Status.multi_select[0].name;
-        result.statusColor = page.properties.Status.multi_select[0].color;
+        result.id = page.id;
         result.summary = page.properties.Summary.rich_text[0].plain_text;
-        result.image = "";
-        result.tags = page.properties.Tags.multi_select.map((tag) => {
-          let _tag = {};
-          _tag.name = tag.name;
-          _tag.color = tag.color;
-          return _tag;
-        });
-
+        result.published = page.properties.Published.date.start;
         return result;
       });
       return {
---
+++ functions/getProjects.js
@@ -0,0 +1,43 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+  const headers = {
+    method: "POST",
+    credentials: "include",
+    headers: {
+      Authorization: "Bearer " + process.env.NotionSecret,
+      "Notion-Version": "2021-05-13",
+    },
+  };
+
+  const url =
+    "https://api.notion.com/v1/databases/" + process.env.ProjectsDB + "/query";
+
+  return fetch(url, headers)
+    .then((res) => res.json())
+    .then((data) => {
+      let pages = data.results.map((page) => {
+        let result = {};
+        result.name = page.properties.Name.title[0].plain_text;
+        result.status = page.properties.Status.multi_select[0].name;
+        result.statusColor = page.properties.Status.multi_select[0].color;
+        result.summary = page.properties.Summary.rich_text[0].plain_text;
+        result.image = "";
+        result.tags = page.properties.Tags.multi_select.map((tag) => {
+          let _tag = {};
+          _tag.name = tag.name;
+          _tag.color = tag.color;
+          return _tag;
+        });
+
+        return result;
+      });
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({ results: pages }),
+      };
+    });
+};
