d2ba6b7a7273bf6893e89ea1b39afa4b99f438b3
patrickjgabr <patrickjgabr@gmail.com>
2021-07-12 14:46:36 +1000

    home updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box, Heading, Markdown } from "grommet";
+import { Box, Heading, Markdown, Text } from "grommet";
 
 function Home(props) {
   const [post, setPost] = useState("");
@@ -23,6 +23,121 @@ function Home(props) {
       margin={{ bottom: "large", top: "48px" }}
     >
       <Markdown>{post}</Markdown>
+      <Box direction="row-responsive" gap="medium">
+        <div>
+          <Heading level={4} margin={{ vertical: "small" }}>
+            Primary Languages
+          </Heading>
+          <Box direction="column" gap="xsmall">
+            <Box direction="row" gap="small">
+              <i class="devicon-javascript-plain colored"></i>
+              <Text>JavaScript</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-python-plain colored"></i>
+              <Text>Python</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-dart-plain colored"></i>
+              <Text>Dart</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-java-plain colored"></i>
+              <Text>Java</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-csharp-plain-wordmark colored"></i>
+              <Text>C#</Text>
+            </Box>
+          </Box>
+        </div>
+        <div>
+          <Heading level={4} margin={{ vertical: "small" }}>
+            {"Technologies"}
+          </Heading>
+          <Box direction="column" gap="xsmall">
+            <Box direction="row" gap="small">
+              <i class="devicon-react-plain colored"></i>
+              <Text>React</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-nodejs-plain colored"></i>
+              <Text>Node.js</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-express-original colored"></i>
+              <Text>Express</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-flutter-plain colored"></i>
+              <Text>Flutter</Text>
+            </Box>
+          </Box>
+        </div>
+
+        <div>
+          <Heading level={4} margin={{ vertical: "small" }}>
+            Tools
+          </Heading>
+          <Box direction="column" gap="xsmall">
+            <Box direction="row" gap="small">
+              <i class="devicon-vscode-plain colored"></i>
+              <Text size="small">VSCode</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-github-plain colored"></i>
+              <Text size="small">Github</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-amazonwebservices-original colored"></i>
+              <Text size="small">AWS</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-heroku-plain colored"></i>
+              <Text size="small">Heroku</Text>
+            </Box>
+          </Box>
+        </div>
+        <div>
+          <Heading level={4} margin={{ vertical: "small" }}>
+            Databases
+          </Heading>
+          <Box direction="column" gap="xsmall">
+            <Box direction="row" gap="small">
+              <i class="devicon-postgresql-plain colored"></i>
+              <Text>PostgresQL</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <i class="devicon-mongodb-plain colored"></i>
+              <Text>MongoDB</Text>
+            </Box>
+          </Box>
+        </div>
+        <div>
+          <Heading level={4} margin={{ vertical: "small" }}>
+            Concepts
+          </Heading>
+          <Box direction="column" gap="xsmall">
+            <Box direction="row" gap="small">
+              <Text size="small">Object Oriented Programming</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <Text size="small">Restful API's</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <Text size="small">{"Scrum/Agile/Waterfall"}</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <Text size="small">{"PERN/MERN Stack"}</Text>
+            </Box>
+            <Box direction="row" gap="small">
+              <Text size="small">
+                {"Serverless Compute (Netlify Function's/AWS Lambda)"}
+              </Text>
+            </Box>
+          </Box>
+        </div>
+      </Box>
     </Box>
   );
 }
